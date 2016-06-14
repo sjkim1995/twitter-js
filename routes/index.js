6837da5dcc53acc93f744131e1726a272df7a8b9
Chris DeMoll <demoll.chris@gmail.com>
2016-06-14 18:16:16 -0400

    form added

--- routes/index.js
+++ routes/index.js
@@ -2,10 +2,22 @@ var express = require('express');
 var router = express.Router();
 // could use one line instead: var router = require('express').Router();
 var tweetBank = require('../tweetBank');
+var bodyParser = require('body-parser');
+
+router.use(bodyParser.urlencoded({ extended: false }));
+
+router.use(bodyParser.json());
 
 router.get('/', function (req, res) {
   var tweets = tweetBank.list();
-  res.render( 'index', { title: 'Twitter.js', tweets: tweets } );
+  res.render( 'index', { showForm: true, title: 'Twitter.js', tweets: tweets } );
+});
+
+router.post('/tweets', function(req, res) {
+  var name = req.body.name;
+  var text = req.body.text;
+  tweetBank.add(name, text);
+  res.redirect('/');
 });
 
 router.get('/users/:name', function (req, res) {
@@ -13,7 +25,7 @@ router.get('/users/:name', function (req, res) {
   var tweets = tweetBank.find( {name: name} );
   // res.render( 'index', { title: 'Twitter.js', tweets: tweets } );
   console.log(tweets);
-  res.render( 'index', { title: 'Twitter.js - Posts by '+ name, tweets: tweets } );
+  res.render( 'index', { showForm: true, title: 'Twitter.js - Posts by '+ name, tweets: tweets, name: name } );
 });
 
 router.get('/tweets/:id', function (req, res) {
@@ -21,11 +33,8 @@ router.get('/tweets/:id', function (req, res) {
   var tweets = tweetBank.find( {id: id} );
   // res.render( 'index', { title: 'Twitter.js', tweets: tweets } );
   console.log(tweets);
-  res.render( 'index', { title: 'Welcome to Twitte', tweets: tweets } );
+  res.render( 'index', { title: 'Welcome to Twitter', tweets: tweets } );
 });
 
-// router.get('/xyz', function(req, res) {
-//   res.sendFile("/Users/christopherdemoll/Fullstack/twitter-js/public/stylesheets/style.css");
-// });
 
 module.exports = router;
